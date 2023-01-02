b4c4b1d7430141126aec405af5c02808a772685e
Vladimir Ilmov <vladimir.ilmov@jetbrains.com>
2023-01-02 02:04:15 +0100

    works for resolution

--- src/main/java/org/jetbrains/teamcity/AssemblePluginMojo.java
+++ src/main/java/org/jetbrains/teamcity/AssemblePluginMojo.java
@@ -1,25 +1,75 @@
 package org.jetbrains.teamcity;
 
-import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugin.PluginConfigurationException;
+import org.apache.maven.artifact.resolver.filter.ArtifactFilter;
+import org.apache.maven.artifact.resolver.filter.ScopeArtifactFilter;
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.plugin.*;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.project.DefaultProjectBuildingRequest;
 import org.apache.maven.project.MavenProject;
-//import org.apache.maven.shared.dependency.graph.DependencyCollectorBuilder;
+import org.apache.maven.project.ProjectBuildingRequest;
+import org.apache.maven.shared.artifact.filter.StrictPatternExcludesArtifactFilter;
+import org.apache.maven.shared.artifact.filter.StrictPatternIncludesArtifactFilter;
+import org.apache.maven.shared.dependency.graph.*;
+import org.apache.maven.shared.dependency.graph.filter.AncestorOrSelfDependencyNodeFilter;
+import org.apache.maven.shared.dependency.graph.filter.AndDependencyNodeFilter;
+import org.apache.maven.shared.dependency.graph.filter.ArtifactDependencyNodeFilter;
+import org.apache.maven.shared.dependency.graph.filter.DependencyNodeFilter;
+import org.apache.maven.shared.dependency.graph.traversal.*;
+import org.eclipse.aether.RepositorySystem;
+import org.eclipse.aether.RepositorySystemSession;
+import org.eclipse.aether.repository.RemoteRepository;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static java.nio.file.Files.exists;
+import static org.apache.maven.artifact.Artifact.SCOPE_COMPILE_PLUS_RUNTIME;
 
 @Mojo(name = "build", requiresDependencyResolution = ResolutionScope.TEST, requiresDependencyCollection = ResolutionScope.TEST)
 public class AssemblePluginMojo extends AbstractMojo {
 
     @Parameter(defaultValue = "${project}", required = true, readonly = true)
     private MavenProject project;
+
+    @Parameter(defaultValue = "${session}", readonly = true, required = true)
+    private MavenSession session;
+
+    /**
+     * Contains the full list of projects in the reactor.
+     */
+    @Parameter(defaultValue = "${reactorProjects}", readonly = true, required = true)
+    private List<MavenProject> reactorProjects;
+
+    @Component
+    private RepositorySystem repositorySystem;
+
+    @Parameter(defaultValue = "${repositorySystem}")
+    private RepositorySystem repositorySystemParam;
+
+    /**
+     * The current repository/network configuration of Maven.
+     */
+    @Parameter(defaultValue = "${repositorySystemSession}")
+    private RepositorySystemSession repoSession;
+
+    /**
+     * The project's remote repositories to use for the resolution of project dependencies.
+     */
+    @Parameter(defaultValue = "${project.remoteProjectRepositories}")
+    private List<RemoteRepository> projectRepos;
+
+    @Parameter(defaultValue = "${mojoExecution}", readonly = true, required = true)
+    private MojoExecution mojoExecution;
     /**
      * Input directory which contains the files that will zipped. Defaults to the
      * build output directory.
@@ -51,22 +101,149 @@ public class AssemblePluginMojo extends AbstractMojo {
 
     @Parameter(defaultValue = "${project.build.outputDirectory}/META-INF/teamcity-plugin.xml", property = "pluginDescriptorPath")
     private String pluginDescriptorPath;
-//    @Component(
-//            hint = "default"
-//    )
-//    private DependencyCollectorBuilder dependencyCollectorBuilder;
+
+    @Component(hint = "default")
+    private DependencyCollectorBuilder dependencyCollectorBuilder;
+
+    @Component(hint = "default")
+    private DependencyGraphBuilder dependencyGraphBuilder;
+
+    private DependencyNode rootNode;
+
+    @Parameter(property = "tokens", defaultValue = "standard")
+    private String tokens;
+
+    @Parameter(property = "includes")
+    private String includes;
+
+    @Parameter(property = "excludes")
+    private String excludes;
+
+    @Parameter(property = "verbose", defaultValue = "false")
+    private boolean verbose;
+
+    @Parameter(property = "unitTest", defaultValue = "${unitTest}")
+    private boolean unitTest;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
         verifyAndPrepareStructure();
+        ArtifactFilter artifactFilter = createResolvingArtifactFilter(SCOPE_COMPILE_PLUS_RUNTIME);
+        ProjectBuildingRequest buildingRequest =
+                new DefaultProjectBuildingRequest(session.getProjectBuildingRequest());
+        buildingRequest.setProject(project);
+        try {
+            String dependencyTreeString;
+
+            if (verbose) {
+                rootNode = dependencyCollectorBuilder.collectDependencyGraph(buildingRequest, artifactFilter);
+                dependencyTreeString = serializeDependencyTree(rootNode);
+            } else {
+                // non-verbose mode use dependency graph component, which gives consistent results with Maven version
+                // running
+                rootNode = dependencyGraphBuilder.buildDependencyGraph(buildingRequest, artifactFilter);
+
+                dependencyTreeString = serializeDependencyTree(rootNode);
+            }
+            getLog().warn(dependencyTreeString);
+        } catch (DependencyGraphBuilderException | DependencyCollectorBuilderException exception) {
+            throw new MojoExecutionException("Cannot build project dependency graph", exception);
+        }
         project.getAttachedArtifacts();
-        System.out.println("1");
     }
 
     private void verifyAndPrepareStructure() throws MojoExecutionException {
         new File(String.valueOf(outputDirectory)).mkdirs();
-        if (!exists(Path.of(pluginDescriptorPath)))
+        if (!exists(Path.of(pluginDescriptorPath)) && !unitTest)
             throw new MojoExecutionException(String.format("`pluginDescriptorPath` must point to teamcity plugin descriptor (%s).", pluginDescriptorPath));
 
+    }
+
+    private ArtifactFilter createResolvingArtifactFilter(String scope) {
+        ArtifactFilter filter;
+
+        // filter scope
+        if (scope != null) {
+            getLog().debug("+ Resolving dependency tree for scope '" + scope + "'");
+
+            filter = new ScopeArtifactFilter(scope);
+        } else {
+            filter = null;
+        }
+
+        return filter;
+    }
+
+    private String serializeDependencyTree(DependencyNode theRootNode) {
+        StringWriter writer = new StringWriter();
+
+        DependencyNodeVisitor visitor = getSerializingDependencyNodeVisitor(writer);
+
+        // TODO: remove the need for this when the serializer can calculate last nodes from visitor calls only
+        visitor = new BuildingDependencyNodeVisitor(visitor);
+
+        DependencyNodeFilter filter = createDependencyNodeFilter();
+
+        if (filter != null) {
+            CollectingDependencyNodeVisitor collectingVisitor = new CollectingDependencyNodeVisitor();
+            DependencyNodeVisitor firstPassVisitor = new FilteringDependencyNodeVisitor(collectingVisitor, filter);
+            theRootNode.accept(firstPassVisitor);
+
+            DependencyNodeFilter secondPassFilter =
+                    new AncestorOrSelfDependencyNodeFilter(collectingVisitor.getNodes());
+            visitor = new FilteringDependencyNodeVisitor(visitor, secondPassFilter);
+        }
+
+        theRootNode.accept(visitor);
+
+        return writer.toString();
+    }
+
+    public DependencyNodeVisitor getSerializingDependencyNodeVisitor(Writer writer) {
+        return new SerializingDependencyNodeVisitor(writer, toGraphTokens(tokens));
+    }
+
+    private SerializingDependencyNodeVisitor.GraphTokens toGraphTokens(String theTokens) {
+        SerializingDependencyNodeVisitor.GraphTokens graphTokens;
+
+        if ("whitespace".equals(theTokens)) {
+            getLog().debug("+ Using whitespace tree tokens");
+
+            graphTokens = SerializingDependencyNodeVisitor.WHITESPACE_TOKENS;
+        } else if ("extended".equals(theTokens)) {
+            getLog().debug("+ Using extended tree tokens");
+
+            graphTokens = SerializingDependencyNodeVisitor.EXTENDED_TOKENS;
+        } else {
+            graphTokens = SerializingDependencyNodeVisitor.STANDARD_TOKENS;
+        }
+
+        return graphTokens;
+    }
+
+    private DependencyNodeFilter createDependencyNodeFilter() {
+        List<DependencyNodeFilter> filters = new ArrayList<>();
+
+        // filter includes
+        if (includes != null) {
+            List<String> patterns = Arrays.asList(includes.split(","));
+
+            getLog().debug("+ Filtering dependency tree by artifact include patterns: " + patterns);
+
+            ArtifactFilter artifactFilter = new StrictPatternIncludesArtifactFilter(patterns);
+            filters.add(new ArtifactDependencyNodeFilter(artifactFilter));
+        }
+
+        // filter excludes
+        if (excludes != null) {
+            List<String> patterns = Arrays.asList(excludes.split(","));
+
+            getLog().debug("+ Filtering dependency tree by artifact exclude patterns: " + patterns);
+
+            ArtifactFilter artifactFilter = new StrictPatternExcludesArtifactFilter(patterns);
+            filters.add(new ArtifactDependencyNodeFilter(artifactFilter));
+        }
+
+        return filters.isEmpty() ? null : new AndDependencyNodeFilter(filters);
     }
 }
--- src/test/java/org/jetbrains/teamcity/AssemblePluginMojoTestCase.java
+++ src/test/java/org/jetbrains/teamcity/AssemblePluginMojoTestCase.java
@@ -1,10 +1,36 @@
 package org.jetbrains.teamcity;
 
+import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.artifact.repository.ArtifactRepositoryPolicy;
+import org.apache.maven.artifact.repository.MavenArtifactRepository;
+import org.apache.maven.artifact.repository.layout.DefaultRepositoryLayout;
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.plugin.Mojo;
+import org.apache.maven.plugin.MojoExecution;
+import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.testing.MojoRule;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.repository.internal.MavenRepositorySystemUtils;
+import org.eclipse.aether.DefaultRepositorySystemSession;
+import org.eclipse.aether.RepositorySystem;
+import org.eclipse.aether.RepositorySystemSession;
+import org.eclipse.aether.artifact.Artifact;
+import org.eclipse.aether.artifact.DefaultArtifact;
+import org.eclipse.aether.connector.basic.BasicRepositoryConnectorFactory;
+import org.eclipse.aether.impl.DefaultServiceLocator;
+import org.eclipse.aether.internal.impl.SimpleLocalRepositoryManagerFactory;
+import org.eclipse.aether.metadata.Metadata;
+import org.eclipse.aether.repository.*;
+import org.eclipse.aether.resolution.ArtifactRequest;
+import org.eclipse.aether.resolution.ArtifactResolutionException;
+import org.eclipse.aether.resolution.ArtifactResult;
+import org.eclipse.aether.spi.connector.RepositoryConnectorFactory;
+import org.eclipse.aether.spi.connector.transport.TransporterFactory;
 import org.junit.Rule;
 import org.junit.Test;
 
 import java.io.File;
+import java.util.List;
 
 import static org.codehaus.plexus.PlexusTestCase.getTestFile;
 import static org.junit.Assert.assertNotNull;
@@ -14,10 +40,6 @@ public class AssemblePluginMojoTestCase {
     @Rule
     public MojoRule rule = new MojoRule();
 
-//    protected void setUp() throws Exception {
-//        super.setUp();
-//    }
-//
     @Test
     public void testSimple() {
         System.out.println(1);
@@ -25,10 +47,41 @@ public class AssemblePluginMojoTestCase {
 
     @Test
     public void testMakeSimpleArtifact() throws Exception {
-        rule.executeMojo(getTestDir("unit/project-to-test"), "build");
+        // setup with pom set BRANCHNAME  set in pom
+        MavenProject project = rule.readMavenProject(getTestDir("unit/project-to-test"));
+
+        // Generate session
+        MavenSession session = rule.newMavenSession(project);
+
+        // add localRepo - framework doesn't do this on its own
+        ArtifactRepository localRepo = createLocalArtifactRepository(new File(getTestDir("unit/project-to-test"), "repo").getCanonicalPath());
+        session.getRequest().setLocalRepository(localRepo);
+        LocalRepository localRepository = createLocalRepository(new File(getTestDir("unit/project-to-test"), "repo"));
+        LocalRepositoryManager lrm = rule.getContainer().lookup(SimpleLocalRepositoryManagerFactory.class).newInstance(session.getRepositorySession(), localRepository);
+        ((DefaultRepositorySystemSession) session.getRepositorySession()).setLocalRepositoryManager(lrm);
+
+        // Generate Execution and Mojo for testing
+        MojoExecution execution = rule.newMojoExecution("build");
+        AssemblePluginMojo mojo = (AssemblePluginMojo) rule.lookupConfiguredMojo(session, execution);
+        mojo.execute();
+
+    }
+
+    private LocalRepository createLocalRepository(File repo) {
+        return new LocalRepository(repo);
     }
 
     private File getTestDir(String pathToBase) {
         return new File(getClass().getClassLoader().getResource(pathToBase).getFile());
+    }
+
+    private ArtifactRepository createLocalArtifactRepository(String localRepoDir) {
+        return new MavenArtifactRepository("local",
+                localRepoDir,
+                new DefaultRepositoryLayout(),
+                new ArtifactRepositoryPolicy( true, ArtifactRepositoryPolicy.UPDATE_POLICY_ALWAYS, ArtifactRepositoryPolicy.CHECKSUM_POLICY_IGNORE ),
+                new ArtifactRepositoryPolicy( true, ArtifactRepositoryPolicy.UPDATE_POLICY_ALWAYS, ArtifactRepositoryPolicy.CHECKSUM_POLICY_IGNORE )
+
+        );
     }
 }
